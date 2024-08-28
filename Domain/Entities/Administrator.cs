3952c5a19f2ee14be3a8c34252a97cf4ad7d6ca9
Marcos Leite <m.abner@live.co.uk>
2024-08-28 00:33:32 -0300

    administrator entity data annotations

--- Domain/Entities/Administrator.cs
+++ Domain/Entities/Administrator.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +9,18 @@ namespace MinimalApi.Domain.Entities
 {
     public class Administrator
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; } = default!;
+
+        [Required]
+        [StringLength(255)]
         public string Email { get; set; } = default!;
+
+        [StringLength(50)]
         public string Password { get; set; } = default!;
+
+        [StringLength(10)]
         public string Profie { get; set; } = default!;
     }
 }
