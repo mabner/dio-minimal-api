a0f8a3896ad43f1bdfd7c78ea36a502506788fdb
Marcos Leite <m.abner@live.co.uk>
2024-08-28 00:33:08 -0300

    DataBaseContext EF class

--- Infrastructure/Db/DataBaseContext.cs
+++ Infrastructure/Db/DataBaseContext.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using MinimalApi.Domain.Entities;
 
 namespace MinimalApi.Infrastructure.Db
 {
     public class DataBaseContext : DbContext
     {
-        
+        public DbSet<Administrator> Administrators { get; set; } = default!;
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("connection string",
+            ServerVersion.AutoDetect("connection string"));
+        }
     }
 }
