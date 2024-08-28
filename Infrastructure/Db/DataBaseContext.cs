ee9ec69ec6147a7620fe3f9ac1c382c96461e8ea
Marcos Leite <m.abner@live.co.uk>
2024-08-28 02:36:50 -0300

    connection string, dependency injection and constructor

--- Infrastructure/Db/DataBaseContext.cs
+++ Infrastructure/Db/DataBaseContext.cs
@@ -9,13 +9,28 @@ namespace MinimalApi.Infrastructure.Db
 {
     public class DataBaseContext : DbContext
     {
+        // Dependency injection ---->
+        private readonly IConfiguration _appSettingsConfig;
+
+        public DataBaseContext(IConfiguration appSettingsConfig)
+        {
+            _appSettingsConfig = appSettingsConfig;
+        }
+
+        // <-----
         public DbSet<Administrator> Administrators { get; set; } = default!;
 
-        protected override void OnConfiguring(
-            DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySql("connection string",
-            ServerVersion.AutoDetect("connection string"));
+            // Passing the config in the contructor
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connString = _appSettingsConfig.GetConnectionString("mysql")?.ToString();
+                if (!string.IsNullOrEmpty(connString))
+                {
+                    optionsBuilder.UseMySql(connString, ServerVersion.AutoDetect(connString));
+                }
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,19 +1,31 @@
-
+using Microsoft.EntityFrameworkCore;
 using MinimalApi.DTOs;
+using MinimalApi.Infrastructure.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Passing teh config with the builder
+builder.Services.AddDbContext<DataBaseContext>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (LoginDTO loginDTO) =>
-{
-    if (loginDTO.Email == "adm@test.com" && loginDTO.Senha == "123456")
-        return Results.Ok("Login com sucesso");
-    else
-        return Results.Unauthorized();
-
-});
-
+app.MapPost(
+    "/login",
+    (LoginDTO loginDTO) =>
+    {
+        if (loginDTO.Email == "adm@test.com" && loginDTO.Password == "123456")
+            return Results.Ok("Login com sucesso");
+        else
+            return Results.Unauthorized();
+    }
+);
 
 app.Run();
