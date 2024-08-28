dec26a25efb1f43222781feed57835f45b97b8ce
Marcos Leite <m.abner@live.co.uk>
2024-08-28 00:14:44 -0300

    changes the password property name in the login dto

--- Domain/DTOs/LoginDTO.cs
+++ Domain/DTOs/LoginDTO.cs
@@ -8,6 +8,6 @@ namespace MinimalApi.DTOs
     public class LoginDTO
     {
         public string Email { get; set; } = default!;
-        public string Senha { get; set; } = default!;
+        public string Password { get; set; } = default!;
     }
 }
