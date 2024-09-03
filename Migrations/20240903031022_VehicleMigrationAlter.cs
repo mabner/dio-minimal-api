b31d02a33bb0d9ef8d6fb8e516abc038af95a990
Marcos Leite <m.abner@live.co.uk>
2024-09-03 00:18:00 -0300

    migrations to correct vehicle model field

---
+++ Migrations/20240903031022_VehicleMigrationAlter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class VehicleMigrationAlter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Vehicles",
+                newName: "Model");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Model",
+                table: "Vehicles",
+                newName: "Name");
+        }
+    }
+}
