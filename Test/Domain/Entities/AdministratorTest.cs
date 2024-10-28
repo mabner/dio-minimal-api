58046cd1f20f81660be203a7600062593dd07a86
Marcos Leite <m.abner@live.co.uk>
2024-10-28 00:42:05 -0300

    tests for validating the administrator fields

---
+++ Test/Domain/Entities/AdministratorTest.cs
@@ -0,0 +1,28 @@
+using MinimalApi.Domain.Entities;
+
+namespace Test.Domain.Entities;
+
+[TestClass]
+public class AdministratorTest
+{
+    [TestMethod]
+    public void TestGetSetProperties()
+    {
+        // Arrange
+        var admin = new Administrator();
+
+        // Act (Set)
+        admin.Id = 1;
+        admin.Email = "admin@test.com";
+        admin.Password = "123456";
+        admin.Profie = "Admin";
+
+        // Assert (Get)
+        Assert.AreEqual(1, admin.Id);
+        Assert.AreEqual("admin@test.com", admin.Email);
+        Assert.AreEqual("123456", admin.Password);
+        Assert.AreEqual("Admin", admin.Profie);
+
+        
+    }
+}
