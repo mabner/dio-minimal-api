a075a0c3a1def3483dc0a642b791e6fc69d5dd8a
Marcos Leite <m.abner@live.co.uk>
2024-11-05 00:23:54 -0300

    new program.cs and configuration service startup

---
+++ API/Startup.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using MinimalApi.Domain.Interfaces;
+using MinimalApi.Domain.Services;
+using MinimalApi.Infrastructure.Db;
+using System.Text;
+
+namespace MinimalApi;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration? Configuration { get; set; }
+
+    public void ConfigurationServices(IServiceCollection services)
+    {
+
+        services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(option =>
+            {
+                option.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateLifetime = true,
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+                };
+            });
+
+        services.AddAuthorization();
+
+        services.AddScoped<IAdministratorService, AdministratorService>();
+        services.AddScoped<IVehicleService, VehicleService>();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "DIO - Minimal API", Version = "v1" });
+            c.AddSecurityDefinition(
+                "Bearer",
+                new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Insert the JWT token:",
+                }
+            );
+            c.AddSecurityRequirement(
+                new OpenApiSecurityRequirement
+                {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+                }
+            );
+        });
+
+        // Passing the config with the builder
+        services.AddDbContext<DataBaseContext>(options =>
+        {
+            options.UseMySql(
+                Configuration.GetConnectionString("MySql"),
+                ServerVersion.AutoDetect(Configuration.GetConnectionString("MySql"))
+            );
+        });
+
+    }
+}
