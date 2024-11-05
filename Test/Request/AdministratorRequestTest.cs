de5e66573db6fd0679ec8c878993d488de7f8d63
Marcos Leite <m.abner@live.co.uk>
2024-11-05 03:52:35 -0300

    Create AdministratorRequestTest.cs

---
+++ Test/Request/AdministratorRequestTest.cs
@@ -0,0 +1,18 @@
+namespace Test.Requests;
+
+[TestClass]
+public class AdministratorRequestTest
+{
+    [TestMethod]
+    public void TestGetSetProperties()
+    {
+        // Arrange
+        
+
+        // Act (Set)
+        
+
+        // Assert (Get)
+        
+    }
+}
