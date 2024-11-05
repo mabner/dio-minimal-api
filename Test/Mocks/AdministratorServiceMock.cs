78cbf6f4830a040a037490b04cc974799f80891e
Marcos Leite <m.abner@live.co.uk>
2024-11-05 03:52:27 -0300

    test setup and administrator service mockup

---
+++ Test/Mocks/AdministratorServiceMock.cs
@@ -0,0 +1,32 @@
+﻿using MinimalApi.Domain.DTOs;
+using MinimalApi.Domain.Entities;
+using MinimalApi.Domain.Interfaces;
+
+namespace Test.Mocks;
+
+internal class AdministratorServiceMock : IAdministratorService
+{
+    private static List<Administrator> admins = new List<Administrator>();
+    public Administrator Add(Administrator administrator)
+    {
+        administrator.Id = admins.Count() + 1;
+        admins.Add(administrator);
+
+        return administrator;
+    }
+
+    public Administrator? GetAdministratorById(int id)
+    {
+        return admins.Find(a => a.Id == id);
+    }
+
+    public List<Administrator> GetAdministrators(int? page)
+    {
+        return admins;
+    }
+
+    public Administrator? Login(LoginDTO loginDTO)
+    {
+        return admins.Find(a => a.Email == loginDTO.Email && a.Password == loginDTO.Password);
+    }
+}
